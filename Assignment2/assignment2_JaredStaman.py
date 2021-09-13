6b685a58344a5cc7cd6f7450c1d525cc7be88521
jstaman5 <jstaman5@gmail.com>
2021-09-13 18:36:30 -0400

    working scatter plot

--- Assignment2/assignment2_JaredStaman.py
+++ Assignment2/assignment2_JaredStaman.py
@@ -4,31 +4,26 @@
 import pandas as pd
 import csv
 import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 def main():
     
-    with open('height_weight.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                line_count += 1
-            else:
-                #heights = row[0]
-                #weights = row[1]
-                sexes = convert_sex(row[2])
-                line_count += 1
-        
+    df = pd.read_csv("height_weight.csv")
+
+    #data = dframe.to_numpy
+
+    height = "Height (in)"
+    weight = " Weight (lbs)"
+    sex = " Sex"
     
+    male = df[df[sex] == " Male"].to_numpy()
+    female = df[df[sex] == " Female"].to_numpy()
 
-
-# convert Male to 1, Female to 0
-def convert_sex(sex):
-    if(sex == " Male"):
-        sex = 1
-    else:
-        sex = 0
+    min_male_height = male[:,0].min()
+    #weight [:,1]
+    #print(min_male_height)
     
-    return sex
+    sns.scatterplot(data = df, x = weight, y = height, hue = sex)
 
+    plt.show()
 main()
