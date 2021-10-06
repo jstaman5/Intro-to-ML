c3ebc82774eb566aab169460eeb65b3db3ed44b9
Jared Staman <jstaman5@gmail.com>
2021-10-05 20:29:45 -0400

    just need last graphs

--- Assignment3/assignment3_JaredStaman.py
+++ Assignment3/assignment3_JaredStaman.py
@@ -15,10 +15,10 @@ def calcDist(point, centroid):
     return math.sqrt( (r-c_r)**2 + (g-c_g)**2 + (b-c_b)**2)
 
 def Kmeans(image, K):
-
+    np.seterr(divide='ignore', invalid='ignore')
     #initialize cluster centroids randomly
     clusters = np.random.randint(0,255, size=(K,3))
-
+    delta = []
     h, w, c = image.shape
     original_img = image.copy()
     copy = image.copy()
@@ -47,25 +47,48 @@ def Kmeans(image, K):
                        # min = d
                        # copy[i][j] = centroid
         
-        delta = []
+        #delta = []
+        tmp = []
         pixels = []
         u = False
         for i in range(K):
-        
             X, Y, c = np.where(copy == clusters[i])
             points = original_img[X, Y]
             #how many pixels are in each centroid
             #print(np.size(X))
             pixels.append(np.size(X))
             new_centroid = np.mean(points, axis = 0)
-            print(new_centroid)
+            #print(new_centroid)
             d = calcDist(new_centroid, clusters[i])
             clusters[i] = new_centroid
-            delta.append(d)
+            #print(d)
+           # if(math.isnan(d)):
+         #       tmp.append(0.0)
+          #  else:
+          #      tmp.append(d)
+            '''
+            #print(tmp)
+            if(i == K - 1 ):
+                d2 = sum(tmp) / len(tmp)
+                delta.append(d2)
+                if (d2 < 1):
+                    stop = True
+                    print(f'iterations: {it}')
+                print(delta)
+            '''
+            #if(i == K - 1):
+            #    if(math.isnan(sum(tmp) / len(tmp))):
+             #       delta.append(0.0)
+             #   else:
+             #       delta.append( sum(tmp) / len(tmp))
+                #print(delta)
+        
+            #delta.append(d)
             if(d > 1):
                 u = True
             
             if u == False:
+                print(f'distance: {d}')
                 print(f'iterations: {it}')
                 stop = True
             
@@ -81,17 +104,20 @@ def Kmeans(image, K):
     
 def main(): 
     #read in image
+    '''
     image = io.imread('baboon.jpg')
     pixels = []
     delta = []
-    new_image, pixels, delta = Kmeans(image, 8)
+    new_image, pixels, delta = Kmeans(image,4)
     total_pixels = sum(pixels)
     pixel_percentage = []
     for idx, p in enumerate(pixels):
-        pixel_percentage.append(p / total_pixels)
+        pixel_percentage.append( (p / total_pixels) * 100)
     print(pixel_percentage)
     print(delta)
-    io.imshow(new_image) 
+    io.imshow(new_image)
+    plt.show()
+    '''
     '''
     f, axarr = plt.subplots(nrows=2, ncols=2, figsize=(15,4))
 
@@ -103,8 +129,73 @@ def main():
 
         axarr[i].imshow(new_image)
     '''
-    
+    '''
+    plt.clf()
+    iterations = []
+    for idx, val in enumerate(delta):
+        iterations.append(idx)
+    plt.axis((0,23, 0, 45))
+    plt.plot(iterations, delta, label = "4")
     plt.show()
+    '''
+
+    image = io.imread('truck.jpg')
+
+    pixels = []
+    deltas = []
+    #f, axarr = plt.subplots(nrows = 2, ncols = 2, figsize=(30,8))
+
+    '''
+    axarr[0,0].imshow(image)
+    axarr[0,0].set_title("Original Image")
+    new_image, delta, pixel = Kmeans(image, 4)
+    axarr[0,1].imshow(new_image)
+    axarr[0,1].set_title("Image Obtained for K = 4")
+    new_image, delta, pixel = Kmeans(image, 16)
+    axarr[1,0].imshow(new_image)
+    axarr[1,0].set_title("Image Obtained for K = 16")
+    new_image, delta, pixel = Kmeans(image, 32)
+    axarr[1,1].imshow(new_image)
+    axarr[1,1].set_title("Image Obtained for K = 32")
+    f.tight_layout()
+    plt.show()
+    '''
+    plt.clf()
+    new_image, pixel, delta = Kmeans(image, 4)
+    x = [x for x in range(5) if x!= 0]
+    #x2 = [x for x in range(17) if x!= 0]
+    #x3 = [x for x in range(33) if x!= 0]
+    print(pixel)
+    plt.subplot(1,3,1)
+    plt.bar(x, pixel)
+    plt.ylabel("Pixels in Cluster")
+    plt.xlabel("K = 4")
+    plt.ylim((0,200000))
+    plt.locator_params(axis="both", integer = True)
+
+    new_image, pixel, delta = Kmeans(image, 16)
+    x2 = [x for x in range(17) if x!= 0]
+    print(pixel)
+    plt.subplot(1,3,2)
+    plt.bar(x2,pixel)
+    plt.xlabel("K = 16")
+    plt.title("Cluster Distributions")
+    plt.ylim((0,200000))
+    plt.locator_params(axis="both", integer = True)
+    
+    new_image, pixel, delta = Kmeans(image, 32)
+    x3 = [x for x in range(33) if x!= 0]
+    print(pixel)
+    plt.subplot(1,3,3)
+    plt.bar(x3,pixel)
+    plt.xlabel("K = 32")
+    plt.ylim((0,200000))
+    plt.locator_params(axis="both", integer = True)
+    plt.show()
+
+
+    #plt.clf()
+    
 
 if __name__ == "__main__":
     main()
