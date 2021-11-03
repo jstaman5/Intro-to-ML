81de7f859d4b951eabcecb21bd4d5c83755faf8e
jstaman5 <jstaman5@gmail.com>
2021-11-03 18:17:27 -0400

    linear working

--- Assignment5/assignment5_JaredStaman.py
+++ Assignment5/assignment5_JaredStaman.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import math
 from numpy.linalg import det, inv
+import matplotlib.pyplot as plt
 
 def main():
 
@@ -76,6 +77,12 @@ def main():
 
         return (-B + math.sqrt(B**2 - 4*A*C)) / (2*A)
 
+    #Used for graphing the gaussian curves
+    def p(x, mu, sigma):
+        vector_exp = np.vectorize(math.exp)
+        z = 1/(math.sqrt(2*np.pi)*sigma)
+        return z*vector_exp(-0.5*((x-mu)/sigma)**2)
+
     def F(x, mu, sigma):
         vector_erf = np.vectorize(math.erf)
         return .5 * (1+vector_erf (((x - mu)/ sigma)/ math.sqrt(2)))
@@ -86,7 +93,7 @@ def main():
 
     #calculate probability errors
     pE_height = (1-F(x_train, male_height_mean, male_height_std)) * male_probability + F(x_train, female_height_mean, female_height_std) * female_probability
-    
+    #pE_height2 = (1-F(threshold_height, male_height_mean, male_height_std)) * male_probability + F(threshold_height, female_height_mean, female_height_std) * female_probability
     pE_weight = (1-F(x_train, male_weight_mean, male_weight_std)) * male_probability + F(x_train, female_weight_mean, female_weight_std) * female_probability
 
     #Classify Test Data
@@ -106,34 +113,67 @@ def main():
     weight_err = w_fp + w_fn / np.size(y_test)
 
     #plots
+    
+    '''hist = plt.histogram(x_train[:,0], bins = 10) #, range = (np.min(x_train[:,0], np.max(x_train[:,0]))))
+    plt.plot(hist)
+    plt.show()'''
 
+    #height graph with histogram on top
+    '''male_height_range = np.array([x for x in range(50, 80)])
+    female_height_range = np.array([x for x in range(50,80)])
+    p1 = p(male_height_range, male_height_mean, male_height_std) * male_probability
+    p2 = p(female_height_range, female_height_mean, female_height_std) * female_probability
+    plt.plot(male_height_range, p1, '-b')
+    plt.plot(female_height_range, p2, '-r')
+    plt.axvline(x = threshold_height, linestyle = 'dashed')
+    plt.show()
+    plt.figure()'''
+    '''
+    #weight graph with histogram on top
+    male_weight_range = np.array([x for x in range(60, 250)])
+    female_weight_range = np.array([x for x in range(60,250)])
+    p1 = p(male_weight_range, male_weight_mean, male_weight_std) * male_probability
+    p2 = p(female_weight_range, female_weight_mean, female_weight_std) * female_probability
+    plt.plot(male_weight_range, p1, '-b')
+    plt.plot(female_weight_range, p2, '-r')
+    plt.axvline(x = threshold_weight, linestyle = 'dashed')
+    plt.show()'''
 
     #linear 2D Bayesian classifier
-    def linear(x, mu, covariance, prior):
+    def linear(mu, mu2, covariance, prior):
         inverse = inv(covariance)
-        w0 = -1/2(np.dot(np.dot(np.transpose(mu), inverse) , mu)) + math.log(prior)
-        w = np.dot(inverse, mu)
-        #g = np.dot(X, w) + w0
-        g = np.dot(np.transpose(w), x) + w0
-        return g
+        w0 = -1/2 * (np.dot(np.dot(np.transpose(mu + mu2), inverse) , (mu-mu2))) + math.log(prior)
+        w = np.dot(inverse, mu-mu2)
+        
+        return np.transpose(w), w0
 
-    def quadratic(x, mu, covariance, prior):
+    def quadratic(mu, mu2, covariance, prior):
         inverse = inv(covariance)
-        w0 = -1/2(np.transpose(mu) * inverse * mu) - ((1/2) * math.log(det(covariance))) + math.log(prior)
-        W = -1/2(inverse)
-        w = inverse * mu
-        g = np.transpose(x) * W * x + np.transpose(w) * x + w0
-        return g
+        w0 = -1/2 * np.dot(np.dot(np.transpose(mu+mu2), inverse) , mu-mu2) - ((1/2) * math.log(det(covariance))) + math.log(prior)
+        W = -1/2 * (inverse)
+        w = np.dot(inverse, mu-mu2)
+    
+        return W.flatten(), w, w0
 
+    pooled_covariance[0][1] = 0
+    pooled_covariance[1][0] = 0
+    
     male_mu = np.transpose(np.array([[male_height_mean, male_weight_mean]]))
     female_mu = np.transpose(np.array([[female_height_mean, female_weight_mean]]))
-    male_linear = linear(x_train, male_mu, pooled_covariance, male_probability)
-    female_linear = linear(x_train, female_mu, pooled_covariance, male_probability)
+    
+    w, w0 = linear(male_mu, female_mu, pooled_covariance, male_probability/female_probability)
+    slope = w[0][0] / w[0][1] * -1
+    #print(slope)
+    b = w0[0][0] / w[0][1] * -1
+    #print(b)
+
+    W, w, w0 = quadratic(male_mu, female_mu, pooled_covariance, male_probability/female_probability)
+    print(W, w, w0)
   
-    pred = np.array([1 if male_linear[i] >= female_linear[i] else 0 for i in range(len(x_train))])
+    '''pred = np.array([1 if male_linear[i] >= female_linear[i] else 0 for i in range(len(x_train))])
     fp, tp, tn, fn = confusion_matrix(y_train, pred).ravel()
     accuracy = (tp + tn) / np.size(pred)
-    err = (fp + fn) / np.size(pred)
+    err = (fp + fn) / np.size(pred)'''
     
     return
 
